30ccc276573c984579448a438197cb5f87acb97d
aleksandrandreichenko <andreichenko@web.de>
2022-10-17 13:42:53 +0300

    K8s imports

--- src/main/kotlin/org/frei/k8sjenkins/K8sJenkinsApplication.kt
+++ src/main/kotlin/org/frei/k8sjenkins/K8sJenkinsApplication.kt
@@ -2,9 +2,11 @@ package org.frei.k8sjenkins
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.Banner
 
 @SpringBootApplication
 class K8sJenkinsApplication
+
 
 fun main(args: Array<String>) {
 	runApplication<K8sJenkinsApplication>(*args)
